63cdfe195356a6772ecaf0a885f3a18de6238d51
Alok Saldanha <aloksaldanha@gmail.com>
2020-08-10 17:53:38 -0400

    #22 fixed failing tests

--- tests/test_dir_util.py
+++ tests/test_dir_util.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import MagicMock, patch
-from cellxgene_gateway.dir_util import render_entry
+from cellxgene_gateway.filecrawl import render_entry
 
 class TestRenderEntry(unittest.TestCase):
     def test_GIVEN_path_both_slash_THEN_view_has_single_slash(self):
@@ -8,6 +8,8 @@ class TestRenderEntry(unittest.TestCase):
                 "path": "/somepath/",
                 "name": "entry",
                 "type": "file",
+                "annotations": [],
+                "children": [],
             }
         rendered = render_entry(entry)
         self.assertIn('view/somepath', rendered)
@@ -16,6 +18,8 @@ class TestRenderEntry(unittest.TestCase):
                 "path": "/somepath",
                 "name": "entry",
                 "type": "file",
+                "annotations": [],
+                "children": [],
             }
         rendered = render_entry(entry)
         self.assertIn('view/somepath', rendered)
@@ -24,6 +28,8 @@ class TestRenderEntry(unittest.TestCase):
                 "path": "somepath/",
                 "name": "entry",
                 "type": "file",
+                "annotations": [],
+                "children": [],
             }
         rendered = render_entry(entry)
         self.assertIn('view/somepath', rendered)
@@ -32,6 +38,8 @@ class TestRenderEntry(unittest.TestCase):
                 "path": "somepath",
                 "name": "entry",
                 "type": "file",
+                "annotations": [],
+                "children": [],
             }
         rendered = render_entry(entry)
         self.assertIn('view/somepath', rendered)
