0eed8bcb306eb4f4b80c2206c4e23ad211754aed
Alok Saldanha <aloksaldanha@gmail.com>
2022-09-05 14:51:01 -0400

    Moved prometheus-flask-exporter into extras_require

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ import sys
 
 from setuptools import find_packages, setup
 
-if sys.version_info < (3, 6):
-    sys.exit("Sorry, Python < 3.6 is not supported")
-
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
@@ -23,33 +20,15 @@ def get_version(rel_path):
         raise RuntimeError("Unable to find version string.")
 
 
-def parse_requirements():
-    reqs = []
-    with open("requirements.txt", "r") as f:
-        for line in f.readlines():
-            reqs.append(line.strip("\n"))
-    return reqs
-
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-install_reqs = parse_requirements()
-
 setup(
-    # mandatory
-    name="cellxgene-gateway",
     # mandatory
     version=get_version("cellxgene_gateway/__init__.py"),
     # mandatory
-    author="Niket Patel, Yohann Potier, Alok Saldanha",
-    author_email="alok.saldanha@novartis.com",
-    description=("Cellxgene Gateway"),
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    keywords="visualization, genomics",
-    url="http://github.com/Novartis/cellxgene-gateway",
+    name="cellxgene-gateway",
     packages=find_packages(),
     package_data={
         "cellxgene_gateway": [
@@ -60,8 +39,4 @@ setup(
         ]
     },
     data_files=[("", ["README.md", "LICENSE"])],
-    install_requires=install_reqs,
-    entry_points={"console_scripts": ["cellxgene-gateway=cellxgene_gateway.cli:run"]},
-    classifiers=["Topic :: Scientific/Engineering :: Visualization"],
-    python_requires=">=3.6",
 )
