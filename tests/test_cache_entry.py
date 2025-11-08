c8056991b0a896422fd986daec6babf457ad61fd
Alok Saldanha <alok@Nutmeg.local>
2025-11-08 10:58:03 -0500

    simplify tests

--- tests/test_cache_entry.py
+++ tests/test_cache_entry.py
@@ -10,6 +10,7 @@ from cellxgene_gateway.cache_key import CacheKey
 from cellxgene_gateway.items.item import ItemType
 from cellxgene_gateway.items.file.fileitem import FileItem
 from cellxgene_gateway.items.file.fileitem_source import FileItemSource
+from cellxgene_gateway.gateway import app
 
 key = CacheKey(
     FileItem("/czi/", name="pbmc3k.h5ad", type=ItemType.h5ad),
@@ -19,12 +20,6 @@ key = CacheKey(
 
 class TestRenderEntry(unittest.TestCase):
     def setUp(self):
-        self._tmpdir = tempfile.mkdtemp()
-        self._cellxgene_data = os.environ.get("CELLXGENE_DATA", "")
-        os.environ["CELLXGENE_DATA"] = self._tmpdir
-
-        from cellxgene_gateway.gateway import app
-
         self.app = app
         self.app_context = self.app.test_request_context()
         self.app_context.push()
@@ -32,12 +27,7 @@ class TestRenderEntry(unittest.TestCase):
 
     def tearDown(self):
         self.app_context.pop()
-        if self._cellxgene_data:
-            os.environ["CELLXGENE_DATA"] = self._cellxgene_data
-        else:
-            del os.environ["CELLXGENE_DATA"]
-        shutil.rmtree(self._tmpdir)
-
+        
     def test_GIVEN_key_and_port_THEN_returns_loading_CacheEntry(self):
         entry = CacheEntry.for_key("some-key", 1)
         self.assertEqual(entry.status, CacheEntryStatus.loading)
